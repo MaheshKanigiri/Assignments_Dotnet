7070478c37a931100db6bead49a228298b984790
Mahesh <mahesh.ey.net@gmail>
2023-02-05 21:48:31 +0530

    collections|Practice

---
+++ Collections/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class Example
+    {
+        public static void Main(string[] args)
+        {
+            // Create a list of strings using collection initializer  
+            var names = new List<string>() { "Sonoo", "Vimal", "Ratan", "Love" };
+
+            // Iterate through the list.  
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
